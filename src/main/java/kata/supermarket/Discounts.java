b0b3397dc3e1d0c8e313cb06832f1b6423aa0ada
GC <gordon_chan@hotmail.co.uk>
2020-09-25 14:42:55 +0100

    added private method that returns a map of the basket sorted into lists by product type

--- src/main/java/kata/supermarket/Discounts.java
+++ src/main/java/kata/supermarket/Discounts.java
@@ -11,6 +11,21 @@ public class Discounts {
         this.basket = new ArrayList<>(basket);
     }
 
+    private Map<String, List<Item>> itemByCategory(){
+        Map<String, List<Item>> mapOfItemsByUnit = new HashMap<>();
+        basket.forEach(item ->{
+            Product product = item.product();
+            if(mapOfItemsByUnit.containsKey(product.type())){
+                mapOfItemsByUnit.get(product.type()).add(item);
+            } else {
+                ArrayList<Item> listOfItems= new ArrayList<>();
+                listOfItems.add(item);
+                mapOfItemsByUnit.put(product.type(), listOfItems);
+            }
+        });
+        return mapOfItemsByUnit;
+    }
+
     public Set<String> discountedOffers(){
         HashSet<String> discountedOffers = new HashSet<>();
         discountedOffers.add("sweet");
