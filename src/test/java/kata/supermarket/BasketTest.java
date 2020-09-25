9b78e769fc42db8b0c9225cd0fac604559b7310b
GC <gordon_chan@hotmail.co.uk>
2020-09-25 15:09:47 +0100

    Add a test case for where discounts apply for sweets

--- src/test/java/kata/supermarket/BasketTest.java
+++ src/test/java/kata/supermarket/BasketTest.java
@@ -29,7 +29,8 @@ class BasketTest {
                 aSingleItemPricedPerUnit(),
                 multipleItemsPricedPerUnit(),
                 aSingleItemPricedByWeight(),
-                multipleItemsPricedByWeight()
+                multipleItemsPricedByWeight(),
+                multipleItemsPricedByWeightWhereDiscount()
         );
     }
 
@@ -40,6 +41,12 @@ class BasketTest {
     private static Arguments multipleItemsPricedByWeight() {
         return Arguments.of("multiple weighed items", "1.85",
                 Arrays.asList(twoFiftyGramsOfAmericanSweets(), twoHundredGramsOfPickAndMix())
+        );
+    }
+
+    private static Arguments multipleItemsPricedByWeightWhereDiscount() {
+        return Arguments.of("multiple weighed items with a discount per kilo", "0.85",
+                Arrays.asList(twoFiftyGramsOfAmericanSweets(), twoHundredGramsOfPickAndMix(), eightHundredGramsOfAmericanSweets())
         );
     }
 
@@ -57,23 +64,27 @@ class BasketTest {
     }
 
     private static Item aPintOfMilk() {
-        return new Product(new BigDecimal("0.49")).oneOf();
+        return new UnitProduct(new BigDecimal("0.49")).oneOf();
     }
 
     private static Item aPackOfDigestives() {
-        return new Product(new BigDecimal("1.55")).oneOf();
+        return new UnitProduct(new BigDecimal("1.55")).oneOf();
     }
 
     private static WeighedProduct aKiloOfAmericanSweets() {
-        return new WeighedProduct(new BigDecimal("4.99"));
+        return new WeighedProduct(new BigDecimal("4.99"), "sweet");
     }
 
     private static Item twoFiftyGramsOfAmericanSweets() {
         return aKiloOfAmericanSweets().weighing(new BigDecimal(".25"));
     }
 
+    private static Item eightHundredGramsOfAmericanSweets() {
+        return aKiloOfAmericanSweets().weighing(new BigDecimal("0.80"));
+    }
+
     private static WeighedProduct aKiloOfPickAndMix() {
-        return new WeighedProduct(new BigDecimal("2.99"));
+        return new WeighedProduct(new BigDecimal("2.99"), "sweet");
     }
 
     private static Item twoHundredGramsOfPickAndMix() {
