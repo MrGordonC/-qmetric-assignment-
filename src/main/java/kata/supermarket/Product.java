3bc561e666ecc50c4b274203539bcc99a480143b
GC <gordon_chan@hotmail.co.uk>
2020-09-25 14:23:13 +0100

    Change to interface, with one getter method returning type

--- src/main/java/kata/supermarket/Product.java
+++ src/main/java/kata/supermarket/Product.java
@@ -1,20 +1,6 @@
 package kata.supermarket;
 
-import java.math.BigDecimal;
 
-public class Product {
-
-    private final BigDecimal pricePerUnit;
-
-    public Product(final BigDecimal pricePerUnit) {
-        this.pricePerUnit = pricePerUnit;
-    }
-
-    BigDecimal pricePerUnit() {
-        return pricePerUnit;
-    }
-
-    public Item oneOf() {
-        return new ItemByUnit(this);
-    }
+public interface Product {
+    String type();
 }
